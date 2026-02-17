ccd87846f1e48d841118d6290b653872aa74e5ff
Anandpatel09 <anandbca2021@gmail.com>
2026-02-17 23:29:42 +0530

    fix: fix database connection

--- backend/controllers/singUp.controller.js
+++ backend/controllers/singUp.controller.js
@@ -4,13 +4,11 @@ const bcrypt = require('bcrypt')
 exports.singUp = async(req,res)=>{
     const {name,email,password}=req.body;
     const hashed=await bcrypt.hash(password,8);
-    // console.log(hashed);
-
+ 
 
     const sql="INSERT INTO users(name,email,password) VALUES(?,?,?)"
     db.query(sql,[name,email,hashed],(err)=>{
-        if(err) res.status(500).json(err);
-        
+        if(err) res.status(500).json(err);   
         res.json({message:"user created successfully"})
     })
 }
